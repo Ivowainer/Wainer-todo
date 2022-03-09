5526fa1db38b1201f5bf62c29c2866b545e37ff4
Ivowainer <ivancamposwainer@gmail.com>
2022-03-09 12:23:18 -0300

    Making ToDo

--- admin/proccess/task.php
+++ admin/proccess/task.php
@@ -1,30 +1,34 @@
 <?php 
-    require './admin/databases.php';
-    $errores = [];
+    //Coneccion base de datos
+    $db = mysqli_connect('localhost', 'root', '', 'dashboard_crud');
+
     $taskName = '';
 
-    if($_POST){
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $taskName = $_POST['taskName'];
 
-        if(strlen($taskName) < 5){
+        /* if(strlen($taskName) < 5){
             $errores[] = 'The task must have at least 5 characters';
-        }
+        } */
 
-        if(empty($errores)){
-            $insert = mysqli_query($db, "INSERT INTO task (taskName)
-            VALUES('$taskName')");
+        if($taskName != ""){
+            $bd = mysqli_connect('localhost', 'root', '', 'dashboard_crud');
+
+            $insert = mysqli_query($bd, "INSERT INTO task (taskName)
+            VALUES('$taskName')");  
+
+            mysqli_close($bd);
         }
     }
+    $result_tasks = mysqli_query($db, "SELECT * FROM task");
 
-    $query = "SELECT * FROM task";
-    $result_tasks = mysqli_query($db, $query);
-
-    while($row = mysqli_fetch_array($result_tasks)):?>
+    while($row = mysqli_fetch_array($result_tasks)){?>
         <div class="task">
-            <p><?php echo $row['taskName'];?></p>
             <p><?php echo $row['id'];?></p>
+            <p><?php echo $row['taskName'];?></p>
         </div>
-    <?php endwhile; ?>
+    <?php } mysqli_free_result($result_tasks); ?>
+    
 
     
     
